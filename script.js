c77b7b07f8ceb94bde906f05766831109525e31b
Okawanoyo <YOUKO.OOKAWA@mail.toyota-body.co.jp>
2025-06-08 15:12:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,13 @@
 //★★★DOM:関数イベントに必要なHTMLの要素からとってくる★★★
 //DOM:HTMLタグに紐づける変数宣言。
 const givenNumbers = document.getElementById("section");
-const oneToTenBox = [1,2,3,4,5,6,7,8,9];
 const checkButton = document.getElementById("checkButton");
 
 window.onload = function() {
-  const givenNumbers = Math.floor(Math.random() * 9);
-  console.log(givenNumbers);
+  let givenNumbers = 1;
+  givenNumbers = Math.floor(Math.random() * 9);
+  console.log(givenNumbers);//ok
+  document.getElementById("section").innerHTML = givenNumbers;
 }
 
-
-
-途中
+//0が表示されてしまう。作成中。
