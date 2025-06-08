9720cd33624e170497b138c1562715ce74648bf9
Okawanoyo <YOUKO.OOKAWA@mail.toyota-body.co.jp>
2025-06-08 16:19:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-'use strict';
-
-//同じ数字が出てしまう
 window.onload = function() {
   let cells = document.querySelectorAll(".tableSection td");
-  for (let i = 0; i < cells.length; i++) {
-    let givenNumbers = Math.floor(Math.random() * 9) + 1; // 1〜9のランダムな数
-    console.log(givenNumbers); // OK
-    cells[i].innerHTML = givenNumbers; // 各セルにランダムな数値を代入
-  }
+  console.log(cells);//Nodeの集合.length があるがarrayじゃないらしい。９。
+  for (let i = 0; i < cells.length; i++) { //0-9までの９つを
+    let givenNumbers = Math.floor(Math.random() * 9) + 1; 
+    const alreadyGotNums = [];
+    alreadyGotNums.push(givenNumbers);
+    //ランダムに取得して整数にして箱に入れる
+    console.log(givenNumbers);
+    if(!alreadyGotNums.includes(givenNumbers)) {// OK
+    cells[i].innerHTML = givenNumbers;
+    }    //それを払いだす。
+    } 
 };
-
-//上記からランダムに数字を消す。
-//レベルが上がるごとにセクションが増えていく。
-//増え方のロジックは１，９，１８、３６？
