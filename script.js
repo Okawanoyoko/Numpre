b24daaa74faf73b043deeea78a48be1b369879b3
Okawanoyo <YOUKO.OOKAWA@mail.toyota-body.co.jp>
2025-06-10 19:52:09 +0900

    Update script.js
    つくりかけ。範囲がおかしい

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-window.onload = function() {
-  let cells = document.querySelectorAll(".tableSection td");
-  console.log(cells);//Nodeの集合.length があるがarrayじゃないらしい。９。
+'use strict'
+
+window.onload = function giveNumber() {
+  let cells = document.querySelectorAll(("#grid1, #grid2, #grid3, #grid4, #grid5, #grid6, #grid7, #grid8, #grid9"));
+  console.log(cells);//Nodeの集合.length があるがarrayじゃないらしい。全9td
+  const alreadyGotNums = [];//既出の数字を入れていく配列
   for (let i = 0; i < cells.length; i++) { //0-9までの９つを
     let givenNumbers = Math.floor(Math.random() * 9) + 1; 
-    const alreadyGotNums = [];
-    alreadyGotNums.push(givenNumbers);
     //ランダムに取得して整数にして箱に入れる
-    console.log(givenNumbers);
-    if(!alreadyGotNums.includes(givenNumbers)) {// OK
-    cells[i].innerHTML = givenNumbers;
-    }    //それを払いだす。
+
+    if(!alreadyGotNums.includes(givenNumbers)) {//もしまだ配列にないなら
+       alreadyGotNums.push(givenNumbers); // 配列に格納して
+       cells[i].innerHTML = givenNumbers;   //かつHTMLに払いだす
+       console.log(alreadyGotNums);//確認用
+    }  
     } 
 };
+
+
+
